4707a4f2870045701f684bcc1c33f9587fe15401
TheYmirsCla <lootstealer56@gmail.com>
2024-11-14 22:36:47 -0500

    Update VersionCompatibility.java

--- VCMVersion/src/main/java/me/wikmor/vcmversion/VersionCompatibility.java
+++ VCMVersion/src/main/java/me/wikmor/vcmversion/VersionCompatibility.java
@@ -4,11 +4,13 @@ import org.bukkit.Bukkit;
 
 public class VersionCompatibility {
 
-    private static final String SERVER_VERSION = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+    public static String getServerVersion() {
+        return Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+    }
 
     public static Class<?> getNMSClass(String className) {
         try {
-            return Class.forName("net.minecraft.server." + SERVER_VERSION + "." + className);
+            return Class.forName("net.minecraft.server." + getServerVersion() + "." + className);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
             return null;
@@ -17,7 +19,7 @@ public class VersionCompatibility {
 
     public static Class<?> getCraftBukkitClass(String className) {
         try {
-            return Class.forName("org.bukkit.craftbukkit." + SERVER_VERSION + "." + className);
+            return Class.forName("org.bukkit.craftbukkit." + getServerVersion() + "." + className);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
             return null;
